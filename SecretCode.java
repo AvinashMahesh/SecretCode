4a37e85341da6ea98ce0d7b687fbc5c32da653b0
Avinash Mahesh <87287679+AvinashMahesh@users.noreply.github.com>
2021-10-21 02:11:24 -0400

    Update SecretCode.java

--- SecretCode.java
+++ SecretCode.java
@@ -12,44 +12,36 @@ System.out.println("Enter 2-12 characters");
 String input = thing.next();
 b = isValidLength(input);
  
-if (b == true)
-{
-tot = CharToAscii(input);
+if (b == true) { 
+ tot = CharToAscii(input); 
+ int a = tot;
+ int c = 0;
  
-int a = tot;
+ if (getNumDigits(tot) % 2 == 1) {
+  tot = tot * 10;
+ }
  
-int c = 0;
+ if(tot <= 4000) {
+  tot = tot * 4;
+ }
  
-if (getNumDigits(tot) % 2 == 1)
-{
-tot = tot * 10;
-}
+ if(getNumDigits(tot) % 2 == 1) {
+  tot = tot / 10;
+  c = tot;
+ }
+ else {
+  c = tot;
+ }
  
-if(tot <= 4000)
-{
-tot = tot * 4;
-}
+ int first = tot/100;
+ int end = tot % 100;
  
-if(getNumDigits(tot) % 2 == 1)
-{
-tot = tot / 10;
-c = tot;
-}
-else
-{
-c = tot;
-}
- 
-int first = tot/100;
-int end = tot % 100;
- 
-if (first <= 65)
-{
-first = first + 61;
-}
+ if (first <= 65) {
+  first = first + 61;
+ }
 if(end <=65)
 {
-end = end + 61;
+ end = end + 61;
 }
 one = getSecretCode (first);
 two = getSecretCode (end);
@@ -60,7 +52,7 @@ System.out.println("Secret Code: " + one + two);
 }
 if(b == false)
 {
-System.out.println("Invalid ");
+ System.out.println("Invalid ");
 }
 }
  
